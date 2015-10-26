5c6568419e71a6473971a341f7c216335d43227d
Eugene Davydenko <etchermail@gmail.com>
2015-10-26 23:25:44 +0300

    delete 2015 config

--- mayaAppDir/2015-x64/python/userSetup.py
+++
@@ -1,9 +0,0 @@
-import maya.cmds as m
-m.evalDeferred('import fxpt.fx_collapseShelfButtons; fxpt.fx_collapseShelfButtons.initCollapse()')
-
-#from fxpt.side_utils import fix_pycharm2maya_clipboard
-#fix_pycharm2maya_clipboard.fix()
-
-
-
-
