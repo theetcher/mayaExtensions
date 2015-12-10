4dcb5d9d2178c9e38738176b5d3242b41f0c1191
Eugene Davydenko <etchermail@gmail.com>
2015-12-10 22:07:02 +0300

    fx_refsystem init on startup, colors, etc.

--- mayaAppDir/2016/python/userSetup.py
+++ mayaAppDir/2016/python/userSetup.py
@@ -1,7 +1,9 @@
 import maya.cmds as m
 
+
 m.evalDeferred('import fxpt.fx_collapse_shelf_buttons; fxpt.fx_collapse_shelf_buttons.initCollapse()')
 m.evalDeferred('from fxpt.fx_outliner import fx_outliner; fx_outliner.run()')
+m.evalDeferred('from fxpt.fx_refsystem import fx_ref_system_init; fx_ref_system_init.init()')
 
 #from fxpt.side_utils import fix_pycharm2maya_clipboard
 #fix_pycharm2maya_clipboard.fix()
